88f555034b11bcd51fe059efcbeb3be468a0cd65
Nikhilreddy447 <128344065+Nikhilreddy447@users.noreply.github.com>
2023-04-15 21:41:13 +0530

    Add files via upload
    added exported file of hms database

---
+++ hms.sql
@@ -0,0 +1,260 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2023 at 06:09 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `did` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `doctorname` varchar(50) NOT NULL,
+  `dept` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`did`, `email`, `doctorname`, `dept`) VALUES
+(1, 'jugug@gmail.com', 'Dr.veena', 'cardiologist');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `pid` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `slot` varchar(50) NOT NULL,
+  `disease` varchar(50) NOT NULL,
+  `time` time NOT NULL,
+  `date` date NOT NULL,
+  `dept` varchar(50) NOT NULL,
+  `number` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`pid`, `email`, `name`, `gender`, `slot`, `disease`, `time`, `date`, `dept`, `number`) VALUES
+(1, 'jugug1@gmail.com', 'jumangi1', 'Male1', 'night1', 'heartdisease1', '09:10:00', '0000-00-00', 'ortho', '5434233554');
+
+--
+-- Triggers `patients`
+--
+DELIMITER $$
+CREATE TRIGGER `PatientDelete` BEFORE DELETE ON `patients` FOR EACH ROW INSERT INTO trigr 
+VALUES(null,old.pid,old.email,
+       old.name,'PATIENT DELETED',NOW())
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `PatientUpadate` AFTER UPDATE ON `patients` FOR EACH ROW INSERT INTO trigr 
+VALUES(null,NEW.pid,NEW.email,NEW.name,'PATIENT UPDATE',NOW())
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `patientinsertion` AFTER INSERT ON `patients` FOR EACH ROW INSERT INTO trigr 
+VALUES(null,NEW.pid,NEW.email,NEW.name,'PATIENT INSERTED',NOW())
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `name`, `email`) VALUES
+(1, 'nikhil', 'aletinikhil@gmail.co'),
+(2, 'azeem', 'azeem@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trigger`
+--
+
+CREATE TABLE `trigger` (
+  `tid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trigr`
+--
+
+CREATE TABLE `trigr` (
+  `tid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `timesstamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `trigr`
+--
+
+INSERT INTO `trigr` (`tid`, `pid`, `email`, `name`, `action`, `timesstamp`) VALUES
+(1, 4, 'jugug@gmail.com', 'gchc', 'PATIENT INSERTED', '2023-04-14 23:01:15'),
+(2, 4, 'jugug@gmail.com', 'gchc', 'PATIENT UPDATE', '2023-04-14 23:03:50'),
+(3, 4, 'jugug@gmail.com', 'gchc', 'PATIENT DELETED', '2023-04-14 23:06:36'),
+(4, 3, 'jugug@gmail.com', 'jumangi', 'PATIENT DELETED', '2023-04-14 23:24:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
+(1, 'veena', 'veena@gmail.com', 'pbkdf2:sha256:260000$WNVfh1LqHWPFSayk$aa968c9e77eda582e8f55feb68c5f65079a6cf52e2740a0bbb0d6b7fd0a154a2'),
+(2, 'nikhil', 'nikhil@gmail.com', 'pbkdf2:sha256:260000$5kdvddJrzfCnxpU8$cd7b9f1df51eefc3ad032960f7acb4738faa65d4358217dddc29e6f2031ff13b'),
+(3, 'abcd', 'abcd@gmail.com', 'pbkdf2:sha256:260000$ToaJUpJPL5nyb2pJ$b8d59e28361486b95286e88d575b90eb6c3ea59157456f861da82316aab32e8f'),
+(4, 'veena', 'veena367@gmail.com', 'pbkdf2:sha256:260000$jjeBBgu7OHxeYq9q$4ae7523d0050162734a1fd39c6f18860e890d0825eef5654cbe0a02f78f094da'),
+(5, 'xbb', 'ZSF@gmail.com', 'pbkdf2:sha256:260000$NG5o48iHYF9AJ74n$7176916c7edba51ac01e522a6c68e571ea48fb416e3ddeacb673a1ab4e7de5a4'),
+(6, 'nihgg', 'jugug@gmail.com', 'pbkdf2:sha256:260000$MW3MQ3t5NS0pCqUy$9a3c575233b9842b9929063b4b5b224a6191e67ae5052b6336d2213fe93d3c73'),
+(7, 'uutfutf', 'gytcy@gmail.com', 'pbkdf2:sha256:260000$6AUfNXADKiHSoaj8$e8006ba7448e350f5207dd4c183c14109adc87751905b6b911604b406ff6e90b');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`did`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trigger`
+--
+ALTER TABLE `trigger`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `trigr`
+--
+ALTER TABLE `trigr`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `did` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `trigger`
+--
+ALTER TABLE `trigger`
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trigr`
+--
+ALTER TABLE `trigr`
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
